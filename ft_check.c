ed6dc46378d2d6b5d492cca2fb0b6d17d0921b0f
bwayfare <bwayfare@student.21-school.ru>
2020-12-09 00:27:13 +0300

    Небльшой рефактор кода

--- ft_check.c
+++ ft_check.c
@@ -54,33 +54,19 @@ long int	 ft_checkwidth(va_list *arg, char *str, p_list *list)
 
 	head = str;
 	if (*str >= '0' && *str <= '9' && list->flag == 0)
-	{
 		list->width = ft_atoi(str);
-		str += ft_goch(str, ".\0");
-		return (str - head);
-	}
 	else if (*str == '*' && list->flag == 0)
-	{
 		list->width = va_arg(*arg, int);
-		str = str + ft_goch(str, ".\0");
-		return (1);
-	}
-	return (1);
+	str += ft_goch(str, ".\0");
+	return (str - head);
 }
 
 long int	ft_checkpon(va_list *arg, char *str, p_list *list, char *head)
 {
 	if (*str >= '0' && *str <= '9')
-	{
 		list->pon = ft_atoi(str);
-		str = str + ft_goch(str, "ducsxX\0");
-		return (str - head);
-	}
 	else if (*str == '*')
-	{
 		list->pon = va_arg(*arg, int);
-		str += ft_goch(str, "ducsxX\0");
-		return (str - head);
-	}
+	str += ft_goch(str, "ducsxX\0");
 	return (str - head);
 }
